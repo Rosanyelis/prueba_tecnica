40b08b096560481eae612b280610ae97badcbbe4
Rosanyelis <40235521+Rosanyelis@users.noreply.github.com>
2021-04-27 00:33:45 -0400

    logueo por medio de api y guardado de token

---
+++ src/views/Bienvenido.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col-6 text-center mt-5">
+        <h1 class="pt-5">Welcome to my application</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row justify-content-md-center">
+            <div class="col-6">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h3 class="text-center mb-3">Iniciar Sesión</h3>
+                        <form v-on:submit.prevent="logueo">
+                            <div class="mb-3">
+                                <label for="correo" class="form-label">Correo Electrónico</label>
+                                <input type="email" v-model="email" class="form-control" id="correo">
+                            </div>
+                            <div class="mb-3">
+                                <label for="contraseña" class="form-label">Contraseña</label>
+                                <input type="password" v-model="password" class="form-control" id="contraseña">
+                            </div>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data: function() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    logueo() {
+      axios.post('http://loyal_api.leackstat.info/api/v1/login', {
+        email: this.email,
+        password: this.password,
+      })
+      .then(data =>{
+        if (data.data.success == true) {
+          //Guardo el token en localstorage
+          localStorage.token = data.data.data.token;
+          this.$router.push('bienvenido');
+        }else{
+          alert('el correo o password son inválidos');
+        }
+      });
+    }
   }
 }
 </script>
